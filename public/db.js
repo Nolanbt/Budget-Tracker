1aaf1307557c362684e9c7de44d80ac25f916480
Nolan Terpstra <nbakert@gmail.com>
2020-09-29 21:52:59 -0500

    deploying

--- public/db.js
+++ public/db.js
@@ -14,7 +14,7 @@ request.onsuccess = function (event) {
 };
 
 request.onerror = function (event) {
-    console.log("Woops! " + event.target.errorCode);
+    console.log("Error: " + event.target.errorCode);
 };
 
 function saveRecord(record) {
